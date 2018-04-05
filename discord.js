658fcb70b86ac4a87a2805e8d426886a5ef6d07d
OxyGG <37843613+OxyGG@users.noreply.github.com>
2018-04-05 21:35:52 +0200

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,3 +1,17 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+bot.on('message', message=> {
+    if (message.content === 'bonjour') {
+      message.reply ('Au revoir!  ')
+    }
+})
+
+bot.login('TOKEN')
+JDA jda = new JDABuilder().setBotToken(Token).setBulkDeleteSplittingEnabled(false).buildBlocking();
 const bot = new Discord.Client()
 
 bot.on('ready', function () {
